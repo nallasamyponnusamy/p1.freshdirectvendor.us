e9841ecb90c2ce65f5852b102582a11fcd3b50f2
Nallasamy Ponnusamy <ponnusamy@fas.harvard.edu>
2013-09-17 12:41:42 -0400

    Adding main(Test) Index Page (PHP)

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World Ponnu!</p>'; 
+// prints something like: Wednesday the 15th
+echo date('l \t\h\e jS');
+?>
+ </body>
+</html>
